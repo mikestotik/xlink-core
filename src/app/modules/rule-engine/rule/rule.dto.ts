c0fd2d6e8c824728f11e450b034771c896c028cd
Mike Static <mike.stotik@alseda.by>
2023-01-21 12:00:20 +0300

    refactoring

--- src/app/modules/rule-engine/rule/rule.dto.ts
+++ src/app/modules/rule-engine/rule/rule.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Transform } from 'class-transformer';
+import { BaseEntityDTO } from '../../../database/entity.class';
+import { Rule } from '../../../interfaces/rule.interface';
+import { UserDTO } from '../../core/user/user.dto';
 
 
 export class CreateRuleDTO {
-
+  title!: string;
+  desc?: string;
 }
 
 
@@ -11,6 +16,13 @@ export class UpdateRuleDTO extends PartialType(CreateRuleDTO) {
 }
 
 
-export class RuleDTO {
+export class RuleDTO extends BaseEntityDTO implements Rule {
+  title!: string;
+  desc?: string;
 
+  @Transform(({ value }) => value.id)
+  step!: UserDTO;
+
+  @Transform(({ value }) => value.id)
+  owner!: UserDTO;
 }
